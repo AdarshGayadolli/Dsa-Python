b6d2a379ca621adb359d98460266d749f7e356fb
Adarsh Gayadolli <gayadolliadarsh@gmail.com>
2026-07-06 10:53:45 +0000

    largest element in an array

---
+++ checkifarrayissorted.py
@@ -0,0 +1,7 @@
+
+nums=[1,2,3,4,5,6,7,8]
+n=len(nums)
+for i in range (0,n-1):
+    if nums[i]>nums[i+1]:
+        return False
+return True
---
+++ largestelementinanarray.py
@@ -0,0 +1,7 @@
+class Sulution:
+    def largest(self,arr):
+        max_num=arr[0]
+        for num in arr:
+            if num >max_num:
+                max_num=num
+            return max_num
